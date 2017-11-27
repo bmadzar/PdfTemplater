5f3863e7f0e990f102ada4a30ba33c23b56dfc5e
Boris Madzar <boris.madzar@gmail.com>
2017-11-27 01:30:01 -0500

    Add cycle validator

---
+++ src/PdfTemplater/Node/CycleValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PdfTemplater\Node;
+
+/**
+ * Class CycleValidator
+ *
+ * A helper class that will check a given Node tree for cycles -- a Node cannot
+ * appear more than once in a given tree.
+ *
+ * @package PdfTemplater\Node
+ */
+class CycleValidator implements Validator
+{
+    /**
+     * @var string[]
+     */
+    private $objectHashes;
+
+    /**
+     * Checks that the given Node tree is free from cycles.
+     *
+     * @param Node $rootNode The root of the Node tree to validate.
+     * @return bool True if validation passed, false otherwise.
+     */
+    public function validate(Node $rootNode): bool
+    {
+        $this->objectHashes = [];
+
+        return $this->validateSubtree($rootNode);
+    }
+
+    /**
+     * Checks the subtree for cycles.
+     *
+     * @param Node $node The root of the subtree.
+     * @return bool True if there are no cycles, false otherwise.
+     */
+    private function validateSubtree(Node $node): bool
+    {
+        $hash = \spl_object_hash($node);
+
+        if (\in_array($hash, $this->objectHashes, true)) {
+            return false;
+        }
+
+        $this->objectHashes[] = $hash;
+
+        foreach ($node->getChildren() as $childNode) {
+            if (!$this->validateSubtree($childNode)) {
+                return false;
+            }
+        }
+        unset($childNode);
+
+        return true;
+    }
+}
