6bc62021f93853f8322d81c6d291a20837ada691
Boris Madzar <boris.madzar@gmail.com>
2018-12-15 17:14:52 -0500

    Box tests

--- tests/src/PdfTemplater/Builder/Basic/BoxTest.php
+++ tests/src/PdfTemplater/Builder/Basic/BoxTest.php
@@ -3,9 +3,11 @@
 namespace PdfTemplater\Builder\Basic;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Runner\Exception;
 
 class BoxTest extends TestCase
 {
+    private const RESOLUTION_CSV_PATH = __DIR__ . '/../../../../data/resolution_tests';
 
     public function testConstruct()
     {
@@ -220,12 +222,12 @@ class BoxTest extends TestCase
         $box->setHeightRelative('test2');
 
         $this->assertSame('test2', $box->getHeightRelative());
-        
+
         $this->expectException(ConstraintException::class);
 
         $box->setHeightRelative('test');
     }
-    
+
     public function testWidthRelative()
     {
         $box = new Box('test');
@@ -240,5 +242,168 @@ class BoxTest extends TestCase
 
         $box->setWidthRelative('test');
     }
+
+    public function testDependencies()
+    {
+        $box = new Box('test');
+
+        foreach (['Right', 'Left', 'Top', 'Bottom', 'Width', 'Height'] as $dim) {
+            $box = new Box('test');
+
+            $box->{'set' . $dim}(1.0);
+            $box->{'set' . $dim . 'Relative'}($dim);
+        }
+        unset($dim);
+
+        $this->assertArraySubset(['Right', 'Left', 'Top', 'Bottom', 'Width', 'Height'], $box->getDependencies());
+        $this->assertArraySubset($box->getDependencies(), ['Right', 'Left', 'Top', 'Bottom', 'Width', 'Height']);
+    }
+
+    public function testAbsoluteIsAlwaysResolved()
+    {
+        $box = new Box('test');
+
+        $this->assertTrue($box->isResolved());
+
+        foreach (['Right', 'Left', 'Top', 'Bottom', 'Width', 'Height'] as $dim) {
+            $box = new Box('test');
+
+            $box->{'set' . $dim}(1.0);
+
+            $this->assertTrue($box->isResolved());
+        }
+        unset($dim);
+
+    }
+
+    public function testRelativeIsNotResolved()
+    {
+        foreach (['Right', 'Left', 'Top', 'Bottom', 'Width', 'Height'] as $dim) {
+            $box = new Box('test');
+
+            $box->{'set' . $dim}(1.0);
+            $box->{'set' . $dim . 'Relative'}('test2');
+
+            $this->assertFalse($box->isResolved());
+        }
+        unset($dim);
+
+    }
+
+    public function testBasicResolution()
+    {
+        $box1 = new Box('test');
+        $box2 = new Box('test2');
+
+        $box1->setRight(1.0);
+        $box1->setRightRelative('test2');
+
+        $box2->setLeft(1.0);
+
+        $this->assertTrue($box2->isResolved());
+        $this->assertFalse($box1->isResolved());
+
+        $box1->resolve($box2);
+        $this->assertTrue($box1->isResolved());
+    }
+
+    public function testResolutionCycleCheckSingle()
+    {
+        $box1 = new Box('test');
+
+        $this->expectException(ConstraintException::class);
+
+        $box1->resolve($box1);
+    }
+
+    public function testResolution()
+    {
+        if (!\is_readable(self::RESOLUTION_CSV_PATH) || !\is_dir(self::RESOLUTION_CSV_PATH)) {
+            throw new Exception('Cannot read CSV data directory.');
+        }
+
+        foreach (\glob(self::RESOLUTION_CSV_PATH . '/*.csv') as $dataFile) {
+            $boxData = $this->loadBoxData($dataFile);
+
+            for ($i = 0, $s = \count($boxData); $i < $s; ++$i) {
+                for ($j = ($i + 1); $j < $s; ++$j) {
+                    /** @var Box $box1 */
+                    $box1 = $boxData[$i]['box'];
+                    /** @var Box $box2 */
+                    $box2 = $boxData[$j]['box'];
+
+                    $box1->resolve($box2);
+                }
+                unset($j, $box1, $box2);
+            }
+            unset($i, $s);
+
+            foreach ($boxData as $boxDatum) {
+                /** @var Box $box */
+                $box = $boxDatum['box'];
+
+                $this->assertTrue($box->isResolved());
+                $this->assertTrue($box->isValid());
+
+                foreach (['Right', 'Left', 'Top', 'Bottom', 'Width', 'Height'] as $dim) {
+                    $this->assertSame($boxDatum['finals'][$dim], $box->{'get' . $dim});
+                }
+                unset($dim);
+            }
+            unset($boxDatum, $box);
+        }
+        unset($dataFile);
+    }
+
+    private function loadBoxData($dataFile)
+    {
+        $fh = \fopen($dataFile, 'r');
+        $header = \fgetcsv($fh);
+
+        if (\array_diff(['finalLeft', 'finalRight', 'finalTop', 'finalBottom', 'finalHeight', 'finalWidth'], $header)) {
+            \fclose($fh);
+            throw new Exception(\sprintf('Skipping data file [%s]; missing final fields.', $dataFile));
+        } elseif (!\in_array('id', $header, true)) {
+            \fclose($fh);
+            throw new Exception(\sprintf('Skipping data file [%s]; missing ID field.', $dataFile));
+        }
+
+        $boxData = [];
+        while ($line = \fgetcsv($fh)) {
+            $line = \array_combine($header, $line);
+
+            $box = new Box($line['id']);
+            $finals = [];
+
+            foreach (['Right', 'Left', 'Top', 'Bottom', 'Width', 'Height'] as $dim) {
+                if (isset($line[\strtolower($dim)])) {
+                    $box->{'set' . $dim}((float)$line[\strtolower($dim)]);
+                }
+
+                if (isset($line[\strtolower($dim) . 'Relative'])) {
+                    $box->{'set' . $dim . 'Relative'}((string)$line[\strtolower($dim)]);
+                }
+
+                $finals[$dim] = (float)$line['final' . $dim];
+            }
+            unset($dim);
+
+            if (isset($line['widthPercentage'])) {
+                $box->setWidthPercentage((float)$line['widthPercentage']);
+            }
+
+            if (isset($line['heightPercentage'])) {
+                $box->setHeightPercentage((float)$line['heightPercentage']);
+            }
+
+            $boxData[] = \compact('box', 'finals');
+        }
+        unset($line, $box, $finals, $header);
+
+        \fclose($fh);
+
+        return $boxData;
+    }
+
 
 }
